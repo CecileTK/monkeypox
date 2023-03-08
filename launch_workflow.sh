ff0519ddf951f0b28e07af7d8970df6810147143
Cécile Tran Kiem <ctrankie@fredhutch.org>
2023-03-08 13:11:28 -0800

    update config

--- launch_workflow.sh
+++ launch_workflow.sh
@@ -1,6 +1,6 @@
 nextstrain build --conda . data/sequences.fasta data/metadata.tsv
 
-nextstrain build --conda  --cpus 1 . results/hmpxv1/aligned.fasta 
-results/hmpxv1/insertions.fasta --configfile config/my_config_hmpxv1.yaml
+#nextstrain build --conda  --cpus 1 . results/hmpxv1/aligned.fasta 
+#results/hmpxv1/insertions.fasta --configfile config/my_config_hmpxv1.yaml
 
-#nextstrain build --conda --cpus 4 . --configfile config/my_config_hmpxv1.yaml
+nextstrain build --conda --cpus 4 . --configfile config/config_hmpxv1_big.yaml
